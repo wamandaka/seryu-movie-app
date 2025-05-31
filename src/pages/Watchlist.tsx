c9e39a7b96baf91df6866e2ada2cfe2f1b12d4c5
wamandaka <wamandaka122@gmail.com>
2025-05-31 18:42:40 +0700

    Update WatchList component to change placeholder text from "No favorites yet." to "No watchlist yet."

--- src/pages/Watchlist.tsx
+++ src/pages/Watchlist.tsx
@@ -30,7 +30,7 @@ const WatchList = () => {
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-16 mt-5">
             {bookmarks.length === 0 ? (
               <p className="col-span-full text-center text-lg mt-10">
-                No favorites yet.
+                No watchlist yet.
               </p>
             ) : (
               bookmarks.map((movie) => (
